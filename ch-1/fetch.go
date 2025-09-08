04be96cbaccc6f65126d7563e3766283e5d1062b
mayur <mayurpothuri@gmail.com>
2025-09-08 22:21:21 +0530

    crude fetch program

---
+++ ch-1/fetch.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			continue
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			continue
+		}
+		fmt.Printf("%s", b)
+	}
+}
