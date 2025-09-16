4a2e95dd76638f352518b2862f7f8707dac5f552
mayur <mayurpothuri@gmail.com>
2025-09-16 21:43:00 +0530

    changed exer_1_2.go file name and added exer_1_3.go which compares strings.Join vs for Loop

---
+++ ch-1/exercise/exer_1_3.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println("Time taken using strings.Join:", time.Now().Sub(start))
+
+	start = time.Now()
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+	fmt.Println("Time taken using for Loop and sep \"\":", time.Now().Sub(start))
+}
