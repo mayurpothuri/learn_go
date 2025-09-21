e7494556040faa8732eb409c06c204188566371a
mayur <mayurpothuri@gmail.com>
2025-09-21 21:25:20 +0530

    modified boiling.go so that fahrenheit to celcius logic is defined once

---
+++ ch-2/ftoc.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g°F is %g°C\n", freezingF, fToC(freezingF))
+	fmt.Printf("%g°F is %g°C\n", boilingF, fToC(boilingF))
+}
